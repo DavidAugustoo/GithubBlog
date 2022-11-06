b3d5dcb9badb0311323a34d19afb5cd1e1f5c038
David Augusto <davidribeiro086@gmail.com>
2022-11-06 01:40:44 -0300

    style: Component SeachInput

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,5 +1,11 @@
 import { Profile } from './components/Profile'
+import { SearchInput } from './components/SearchInput/index'
 
 export function Blog() {
-  return <Profile />
+  return (
+    <>
+      <Profile />
+      <SearchInput postsLength={10} />
+    </>
+  )
 }
