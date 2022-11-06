4e2e6e14df7c5390c0435662652f66ce74ab4cdd
David Augusto <davidribeiro086@gmail.com>
2022-11-06 01:50:15 -0300

    style: Component Post

---
+++ src/pages/Blog/components/Post/index.tsx
@@ -0,0 +1,18 @@
+import { PostContainer } from './styles'
+
+export function Post() {
+  return (
+    <PostContainer to="#">
+      <div>
+        <strong>Javascript data Structure</strong>
+        <span>1 dia</span>
+      </div>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident
+        ipsum quia praesentium consequuntur ullam saepe? Aliquam fugiat
+        accusamus voluptate odio autem tempore debitis, obcaecati provident! Eum
+        fugiat assumenda doloremque maxime
+      </p>
+    </PostContainer>
+  )
+}
--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,11 +1,19 @@
+import { Post } from './components/Post'
 import { Profile } from './components/Profile'
 import { SearchInput } from './components/SearchInput/index'
+import { PostsListContainer } from './styles'
 
 export function Blog() {
   return (
     <>
       <Profile />
       <SearchInput postsLength={10} />
+      <PostsListContainer>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </PostsListContainer>
     </>
   )
 }
